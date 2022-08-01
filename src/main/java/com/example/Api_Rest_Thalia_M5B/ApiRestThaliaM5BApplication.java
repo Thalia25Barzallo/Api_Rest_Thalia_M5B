2b269b784a6ea402ee6c14b5569d8146ce49b6df
Thalia Barzallo <89756434+Thalia25Barzallo@users.noreply.github.com>
2022-08-01 09:43:54 -0500

    Creacion

---
+++ src/main/java/com/example/Api_Rest_Thalia_M5B/ApiRestThaliaM5BApplication.java
@@ -0,0 +1,13 @@
+package com.example.Api_Rest_Thalia_M5B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestThaliaM5BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestThaliaM5BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Api_Rest_Thalia_M5B/ApiRestThaliaM5BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_Rest_Thalia_M5B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestThaliaM5BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
